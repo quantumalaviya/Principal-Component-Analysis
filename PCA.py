71b41dac12240343577314e646411fa699c39b27
quantumalaviya <45961148+quantumalaviya@users.noreply.github.com>
2020-10-08 12:50:29 +0530

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -1,10 +1,20 @@
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
+'''
 #Example X
 X = np.array([[1, 2, 3, 7, 5],
               [4, 5, 6, 7, 1],
               [7, 8, 9, 9, 8]])
+'''
+
+m = int(input("Number of rows: ")) 
+X = []
+
+for _ in range(m):
+    row = list(map(int, input().split(" ")))
+    X.append(row)
+
 
 #scaling makes X a singular matrix, however SVD will exist.
 scaler = MinMaxScaler()
@@ -32,3 +42,8 @@ X_decode = np.dot(C, D.T)
 
 #escale back to original data
 X_decode = scaler.inverse_transform(X_decode)
+
+print("Encoding...")
+print(C)
+print("Decoding...")
+print(X_decode)
